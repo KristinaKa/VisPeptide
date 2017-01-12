8dacc3b60211add84c57ff7b4148acef5aa025e4
Lisa Perus <lperus@emi.u-bordeaux1.fr>
2017-01-12 14:52:49 +0100

    First Commit

---
+++ src/spc/main.cpp
@@ -0,0 +1,11 @@
+#include "speptide.h"
+
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+
+	launch_speptide(argc,argv);
+
+
+}
---
+++ src/spc/speptide.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <stdexcept>
+#include "time.h"
+
+#include "../ini/iniparser.h"
+#include "spectra.h"
+#include "mparser.h"
+#include "result.h"
+#include "scoring.h"
+
+#ifndef SPEPTIDE_H
+#define SPEPTIDE_H
+
+
+int launch_speptide (int argc, char *argv[]);
+
+
+
+#endif
